8ea17d032bc2aa53c960638711e9a75eac655a0b
FelipeSantCorreia <135247146+FelipeSantCorreia@users.noreply.github.com>
2024-05-09 08:53:23 -0300

    definitivo

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/android.png'
         },
         {
-            name: 'git',
+            name: 'chrome',
             img: 'images/chrome.png'
         },
         {
@@ -30,8 +30,36 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/android.png'
         },
         {
-
-            
+            name:'chrome',
+            img: 'images/chrome.png'
+        },
+        {
+            name: 'git',
+            img: 'images/git.png'
+        },
+        {
+            name: 'stackoverflow',
+            img: 'images/stackoverflow.png'
+        },
+        {
+            name: 'linux',
+            img: 'images/linux.png'
+        },
+        {
+            name: 'github',
+            img: 'images/github.png'
         }
     ]
+    //embaralhar todas as cartas
+    cards.sort(() => 0,5 - Math.random())
+    //recuperar elementos
+    const board = document.querySelector('.board')
+    const resultView = document.querySelector('#result')
+    let cardsChosen = []//cartas escolhidas
+    let cardsChosenld = []//ids das cartas escolhidas para caso de click na mesma imagem
+    let cardsWon = []//cartas combinadas
+    //criar o quadro de cartas
+    function createBoard(){
+        
+    }
 });
