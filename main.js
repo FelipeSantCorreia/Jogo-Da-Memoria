1c84b023e7e17da7b6785d991efecdda00277dc1
FelipeSantCorreia <135247146+FelipeSantCorreia@users.noreply.github.com>
2024-05-09 08:40:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //opções de cartas
+    const cards = [
+        {
+            name: 'android',
+            img: 'images/android.png'
+        },
+        {
+            name: 'git',
+            img: 'images/chrome.png'
+        },
+        {
+            name: 'git',
+            img: 'images/git.png'
+        },
+        {
+            name: 'stackoverflow',
+            img: 'images/stackoverflow.png'
+        },
+        {
+            name: 'linux',
+            img: 'images/linux.png'
+        },
+        {
+            name: 'github',
+            img: 'images/github.png'
+        },
+        {
+            name: 'android',
+            img: 'images/android.png'
+        },
+        {
+
+        }
+    ]
+});
