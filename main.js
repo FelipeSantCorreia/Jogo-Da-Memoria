0be8b9ca8ec388662e5d4af00478bc207b51129e
FelipeSantCorreia <135247146+FelipeSantCorreia@users.noreply.github.com>
2024-05-09 09:09:07 -0300

    definitivo

--- main.js
+++ main.js
@@ -60,6 +60,30 @@ document.addEventListener('DOMContentLoaded', () => {
     let cardsWon = []//cartas combinadas
     //criar o quadro de cartas
     function createBoard(){
-        
+        for (let i = 0; i< cards.length; i++){
+            const card = document.createElement('img')
+            card.setAttribute('src','images/board.png')
+            card.setAttribute('data-id', i)
+            card.addEventListener('click', flipCard)
+            board.appendChild(card)
+        }
+
+    }
+    //checagem de combinações
+    function checkForMatch(){
+        const cards = document.querySelectorAll('img')
+        const optionOneld = cardsChosenld[0]
+        const optionTwold = cardsChosenld[1]
+
+        //verificar clique na mesma imagem
+        if(optionOneld == optionTwold){
+            cards[optionOneld].setAttribute('src','images/board.png')
+            cards[optionTwold].setAttribute('src','images/board.png')
+            alert('Você clicou na mesma imagem')
+        }
+        //verificar combinações se click em imagens diferentes
+        else if (cardsChosen[0] === cardsChosen[1]){
+            alert('')
+        }
     }
 });
