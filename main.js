60b6f71e189921a49bdbfc27d568a9eb051c97a7
FelipeSantCorreia <135247146+FelipeSantCorreia@users.noreply.github.com>
2024-05-16 07:49:03 -0300

    salvo

--- main.js
+++ main.js
@@ -1,89 +1,125 @@
 document.addEventListener('DOMContentLoaded', () => {
-    //opções de cartas
-    const cards = [
+    document.addEventListener('DOMContentLoaded', () => {
+        //opções de cartas
+        const cards = [
         {
-            name: 'android',
-            img: 'images/android.png'
+        name: 'android',
+        img: 'images/android.png'
         },
         {
-            name: 'chrome',
-            img: 'images/chrome.png'
+        name: 'chrome',
+        img: 'images/chrome.png'
         },
         {
-            name: 'git',
-            img: 'images/git.png'
+        name: 'git',
+        img: 'images/git.png'
         },
         {
-            name: 'stackoverflow',
-            img: 'images/stackoverflow.png'
+        name: 'stackoverflow',
+        img: 'images/stackoverflow.png'
         },
         {
-            name: 'linux',
-            img: 'images/linux.png'
+        name: 'linux',
+        img: 'images/linux.png'
         },
         {
-            name: 'github',
-            img: 'images/github.png'
+        name: 'github',
+        img: 'images/github.png'
+        },
+        
+        {
+        name: 'android',
+        img: 'images/android.png'
         },
         {
-            name: 'android',
-            img: 'images/android.png'
+        name: 'chrome',
+        img: 'images/chrome.png'
         },
         {
-            name:'chrome',
-            img: 'images/chrome.png'
+        name: 'git',
+        img: 'images/git.png'
         },
         {
-            name: 'git',
-            img: 'images/git.png'
+        name: 'stackoverflow',
+        img: 'images/stackoverflow.png'
         },
         {
-            name: 'stackoverflow',
-            img: 'images/stackoverflow.png'
+        name: 'linux',
+        img: 'images/linux.png'
         },
         {
-            name: 'linux',
-            img: 'images/linux.png'
-        },
-        {
-            name: 'github',
-            img: 'images/github.png'
+        name: 'github',
+        img: 'images/github.png'
         }
-    ]
-    //embaralhar todas as cartas
-    cards.sort(() => 0,5 - Math.random())
-    //recuperar elementos
-    const board = document.querySelector('.board')
-    const resultView = document.querySelector('#result')
-    let cardsChosen = []//cartas escolhidas
-    let cardsChosenld = []//ids das cartas escolhidas para caso de click na mesma imagem
-    let cardsWon = []//cartas combinadas
-    //criar o quadro de cartas
-    function createBoard(){
-        for (let i = 0; i< cards.length; i++){
-            const card = document.createElement('img')
-            card.setAttribute('src','images/board.png')
-            card.setAttribute('data-id', i)
-            card.addEventListener('click', flipCard)
-            board.appendChild(card)
+        ]
+        
+        //embaralhar todas as cartas
+        cards.sort(() => 0.5 - Math.random())
+        
+        //recuperar elementos
+        const board = document.querySelector('.board')
+        const resultView = document.querySelector('#result')
+        let cardsChosen = [] //cartas escolhidas
+        let cardsChosenId = [] //ids das cartas escolhidas para caso de click na mesma imagem
+        let cardsWon = [] //cartas combinadas
+        
+        //criar o quadro de cartas
+        function createBoard() {
+        for (let i = 0; i < cards.length; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'images/board.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        board.appendChild(card)
         }
-
-    }
-    //checagem de combinações
-    function checkForMatch(){
+        }
+        
+        //checagem de combinações
+        function checkForMatch() {
         const cards = document.querySelectorAll('img')
-        const optionOneld = cardsChosenld[0]
-        const optionTwold = cardsChosenld[1]
-
+        const optionOneId = cardsChosenId[0]
+        const optionTwoId = cardsChosenId[1]
+        
         //verificar clique na mesma imagem
-        if(optionOneld == optionTwold){
-            cards[optionOneld].setAttribute('src','images/board.png')
-            cards[optionTwold].setAttribute('src','images/board.png')
-            alert('Você clicou na mesma imagem')
+        if(optionOneId == optionTwoId) {
+        cards[optionOneId].setAttribute('src', 'images/board.png')
+        cards[optionTwoId].setAttribute('src', 'images/board.png')
+        alert('Você clicou na mesma imagem')
+        
         }
-        //verificar combinações se click em imagens diferentes
-        else if (cardsChosen[0] === cardsChosen[1]){
-            alert('')
+        //verificar combinação se click em imagens diferentes
+        else if (cardsChosen[0] === cardsChosen[1]) {
+        alert('Você encontrou uma combinação')
+        cards[optionOneId].setAttribute('src', 'images/check.png')
+        cards[optionTwoId].setAttribute('src', 'images/check.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+        } else {
+        cards[optionOneId].setAttribute('src', 'images/board.png')
+        cards[optionTwoId].setAttribute('src', 'images/board.png')
+        alert('Errou, tente novamente')
         }
-    }
-});
+        cardsChosen = []
+        cardsChosenId = []
+        //mostrar placar
+        resultView.textContent = 'Pares Encontrados: '+cardsWon.length
+        if (cardsWon.length === cards.length/2) {
+        resultView.textContent = 'Parabéns! Você conseguiu encontrar todas as cartas'
+        }
+        }
+        
+        //virar as cartas
+        function flipCard() {
+        let cardId = this.getAttribute('data-id')
+        cardsChosen.push(cards[cardId].name)
+        cardsChosenId.push(cardId)
+        this.setAttribute('src', cards[cardId].img)
+        
+        if (cardsChosen.length ===2) {
+        setTimeout(checkForMatch, 500)
+        }
+        }
+        
+        createBoard()
+        })
